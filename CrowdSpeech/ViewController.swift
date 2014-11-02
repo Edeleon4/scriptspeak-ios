5f64f3fed43575e45feca78180bab10b0924c373
Your Namels <edeleon4@mit.edu>
2014-11-02 00:45:36 -0400

    Initial Commit

---
+++ CrowdSpeech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrowdSpeech
+//
+//  Created by Eduardo de Leon on 11/2/14.
+//  Copyright (c) 2014 Eduardo de Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
